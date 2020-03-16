92ef48ca8dbbf07a1311ee4fe65fc4e160201dc9
Yasuhiro Kasama <yasuhiro.kasama7@gmail.com>
2020-03-16 20:43:07 +0900

    consultationテーブルを作成

---
+++ db/migrate/20200316112612_create_consultaitons.rb
@@ -0,0 +1,19 @@
+class CreateConsultaitons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :consultaitons do |t|
+      t.string :name, null: false
+      t.string :cell_number
+      t.string :tel_number
+      t.string :email
+      t.date :birthday
+      t.integer :sex
+      t.integer :type, null: false
+      t.datetime :visit_at, null: false
+
+      t.timestamps
+    end
+    add_index :consultaitons, [:visit_at, :type]
+    add_index :consultaitons, :visit_at
+    add_index :consultaitons, :type
+  end
+end
